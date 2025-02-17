80f4b81923190516d88ff1822ce71383a9d067dd
Rebeca Davila <rebedavilaaraiza@gmail.com>
2025-02-17 14:44:28 -0600

    Añadi una suma y resta

--- operations.py
+++ operations.py
@@ -2,7 +2,9 @@ a = int(input(“Give me the first number:”))
 b = int(input(“Give me the second number:”))
 
 # Display the addition
+print(a + b)
 
 # Display the subtraction
+print(a - b)
 
 # Display multiplicatioin
